28e13c2719b77fb09208a394109b8f7c32b80563
shurimpee <soooojip@gmail.com>
2022-05-04 00:01:23 +0900

    chore: 주석추가, 코드정리

--- commands/latest.js
+++ commands/latest.js
@@ -2,13 +2,13 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require("discord.js");
 
 module.exports = {
+    // command area
     data: new SlashCommandBuilder()
         .setName("뉴스검색")
         .setDescription("뉴스를 검색하는 새우.")
         .addStringOption((option) => option.setName("키워드").setDescription("키워드를 입력하새우.").setRequired(true)),
 
     async execute(interaction) {
-        // const { Naver_Client_Id, Naver_Client_Secret } = require('../config.json');
         const rt = require("../functions/removeTags.js");
         const gn = require("../functions/getNews.js");
 
@@ -16,9 +16,10 @@ module.exports = {
 
         await interaction.deferReply();
 
-        let data = await gn.getNews(keyword);
+        let data = await gn.getNews(keyword); // api call
 
         if (data.total != 0) {
+            // select menus area
             const row = new MessageActionRow().addComponents(
                 new MessageSelectMenu()
                     .setCustomId("select")
@@ -74,21 +75,21 @@ module.exports = {
                             description: "This is also a description",
                             value: "9",
                         },
-                    ])
-            );
-        
+                    ]) // add options
+            ); // end of row
+
+            // get selected value area
             const filter = (interaction) => {
-                return interaction.customId === 'select';
-            }
+                return interaction.customId === "select";
+            };
 
             const collector = interaction.channel.createMessageComponentCollector({
                 filter,
-                time : 60 * 1000, 
+                time: 60 * 1000,
             });
 
             collector.on("collect", async (interaction) => {
-                
-                if(await interaction.customId === 'select') {
+                if ((await interaction.customId) === "select") {
                     const selectedValue = parseInt(interaction.values[0]);
                     console.log(selectedValue);
 
@@ -96,36 +97,59 @@ module.exports = {
                         .setColor("#2DB400")
                         .setTitle(rt.removeTags(data.items[selectedValue].title))
                         .setURL(data.items[selectedValue].link)
-                        // .setAuthor({ name : "`" + keyword + "` 로 검색한 결과입니다."})
-                        .addFields({ name: "ㅤ", value: rt.removeTags(data.items[selectedValue].description) })
-                        .setFooter({ text: "Published :: " + data.items[selectedValue].pubDate });
+                        .addFields({ 
+                            name: "ㅤ", 
+                            value: rt.removeTags(data.items[selectedValue].description) 
+                        })
+                        .setFooter({ 
+                            text: "Published :: " + data.items[selectedValue].pubDate 
+                        });
 
-                    await interaction.update({ content: ":mag: `" + keyword + "` 로 검색한 결과입니다.", embeds: [embed], components: [row] });
-                }
-            })
+                    await interaction.update({ 
+                        content: ":mag: `" + keyword + "` 로 검색한 결과입니다.", 
+                        embeds: [embed], 
+                        components: [row] 
+                    });
+                } // if
+            }); // end of collector
 
+            // default embed area
             let embed = new MessageEmbed()
                 .setColor("#2DB400")
                 .setTitle(rt.removeTags(data.items[0].title))
                 .setURL(data.items[0].link)
                 // .setAuthor({ name : "`" + keyword + "` 로 검색한 결과입니다."})
-                .addFields({ name: "ㅤ", value: rt.removeTags(data.items[0].description) })
-                .setFooter({ text: "Published :: " + data.items[0].pubDate });
-            
-            await interaction.editReply({ content: ":mag: `" + keyword + "` 로 검색한 결과입니다.", embeds: [embed], components: [row] });
+                .addFields({ 
+                    name: "ㅤ", 
+                    value: rt.removeTags(data.items[0].description) 
+                })
+                .setFooter({ 
+                    text: "Published :: " + data.items[0].pubDate 
+                });
 
-            
+            await interaction.editReply({ 
+                content: ":mag: `" + keyword + "` 로 검색한 결과입니다.", 
+                embeds: [embed], 
+                components: [row] 
+            });
         } else {
+            // error(no result matches) handling area
             const embed = new MessageEmbed()
                 .setColor("#EF5350")
                 .setTitle(":warning: 이런! 새우가 길을 잃었네요...")
                 .setThumbnail(
                     "https://w.namu.la/s/4c27296bd851d7b269203ec98248c5d5a4d7d1bc430c3d2ef225d29d69979d47efb449009f6eeecd4789a192dd089660f0be23c680024acade9eba25341e3545960dc249d9e9c7754e7203a4301ec77d"
                 )
-                .addFields({ name: "검색결과를 찾을 수 없습니다.", value: "입력한 키워드 : `" + keyword + "`" })
-                .setFooter({ text: "올바른 키워드를 입력했나요?", iconURL: "https://images.emojiterra.com/twitter/v13.1/512px/1f50d.png" });
+                .addFields({ 
+                    name: "검색결과를 찾을 수 없습니다.", 
+                    value: "입력한 키워드 : `" + keyword + "`" 
+                 })
+                .setFooter({ 
+                    text: "올바른 키워드를 입력했나요?", 
+                    iconURL: "https://images.emojiterra.com/twitter/v13.1/512px/1f50d.png" 
+                });
 
             await interaction.editReply({ embeds: [embed] });
-        }
-    },
-};
+        } // if-else
+    }, // end of execute
+}; // module.exports

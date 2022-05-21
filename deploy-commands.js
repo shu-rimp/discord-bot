4f43ad460ee98e252089fb21dbd5a70ab63deb79
shu-rimp <soooojip@gmail.com>
2022-05-21 20:59:30 +0900

    feat: 글로벌 커멘드 등록

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,8 +13,12 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
+// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+// 	.then(() => console.log('Successfully registered application commands.'))
+// 	.catch(console.error);
+
+rest.put(Routes.applicationCommands(clientId), { body: commands })
+	.then(() => console.log('Successfully registered global application commands.'))
 	.catch(console.error);
 
 console.log(commands);
